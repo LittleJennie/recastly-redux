e30032bfdecd6bbabf219632d38e194946355792
littlejennie <jenniezeng1@gmail.com>
2019-06-12 15:38:09 -0700

    implemented Action dispatchers and curVideo reducer

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,12 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  return () => ({
+    type: 'CHANGE_VIDEO_LIST,CHANGE_VIDEO',
+    videos: searchYouTube({YOUTUBE_API_KEY, q}, changeVideoList) // the state in store
+
+  })
 };
 
 export default handleVideoSearch;
