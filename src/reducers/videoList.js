0cc0f9517bf8c1b493d781737302b13c4b00d83c
littlejennie <jenniezeng1@gmail.com>
2019-06-12 16:48:00 -0700

    rootreducer function implemented

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,14 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (prevState = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
+
+
+  if (action.type === 'CHANGE_VIDEO_LIST') {
+    return action.videos;
+  } else {
+    return prevState;
+  }
 };
 
 export default videoListReducer;
